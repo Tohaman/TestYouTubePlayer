d73582cffa1f163ff0f40a83cd186a8f96191583
TohaRG2 <avrozovu@yandex.ru>
2019-12-30 22:56:44 +0300

    Initial commit

---
+++ app/src/main/java/ru/tohaman/testyoutubeplayer/MainActivity.kt
@@ -0,0 +1,34 @@
+package ru.tohaman.testyoutubeplayer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.YouTubePlayer
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.listeners.AbstractYouTubePlayerListener
+import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.views.YouTubePlayerView
+import ru.tohaman.testyoutubeplayer.ui.main.SectionsPagerAdapter
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val sectionsPagerAdapter = SectionsPagerAdapter(this, supportFragmentManager)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        tabs.setupWithViewPager(viewPager)
+
+
+//        val youTubePlayerView: YouTubePlayerView = findViewById(R.id.youtube_player_view)
+//        val fab: FloatingActionButton = findViewById(R.id.fab)
+//
+//        fab.setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                .setAction("Action", null).show()
+//        }
+    }
+}
